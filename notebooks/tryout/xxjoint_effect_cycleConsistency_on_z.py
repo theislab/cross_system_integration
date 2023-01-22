abba61a01d773d803dfc385071303f4a1b5a15e6
Hrovatin <karin.hrovatin@gmail.com>
2023-01-22 22:09:48 +0100

    z_dist_cycle with standardised MSE

--- notebooks/tryout/xxjoint_effect_cycleConsistency_on_z.py
+++ notebooks/tryout/xxjoint_effect_cycleConsistency_on_z.py
@@ -80,6 +80,9 @@ adata_training = XXJointModel.setup_anndata(
     categorical_covariate_keys=['study_sample'],
 )
 
+# %% [markdown]
+# ### MSE loss
+
 # %%
 models={}
 
@@ -177,6 +180,107 @@ ax[1].set_title('entropy over cts')
 
 # %% [markdown]
 # TOD: Use correlation for latent space- MSE not meaningful
+
+# %%
+for weight,embed in embeds.items():
+    # Minmax scale the embed comonents so that perceived var is not affected by range
+    x=pd.DataFrame(minmax_scale(embed),index=adata_training.obs_names)
+    #var_idx=x.var().sort_values(ascending=False).index
+    x['system']=adata_training[x.index,:].obs['system']
+    x=x.groupby('system').mean()
+    #x=x[var_idx]
+    g=sb.clustermap(x,cmap='cividis',figsize=(7,1),#col_cluster=False
+                   )
+    g.fig.suptitle(weight)
+
+
+# %% [markdown]
+# ### MSE loss on standardised data
+
+# %%
+models={}
+
+# %%
+# Models with different cycle consistency weights
+for weight in [0.0,1.0,5.0,100.0]:
+    # Compute model only if not yet computed
+    if weight not in models:
+        model = XXJointModel(adata=adata_training, z_dist_metric = 'MSE_standard')
+        model.train(max_epochs=40,
+                   plan_kwargs={'loss_weights':dict(
+                    kl_weight= 1.0,
+                    kl_cycle_weight = 0,
+                    reconstruction_weight= 1.0,
+                    reconstruction_mixup_weight = 0,
+                    reconstruction_cycle_weight= 0,
+                    z_distance_cycle_weight = weight,
+                    translation_corr_weight = 0,
+                    z_contrastive_weight = 0,
+                   )})
+        models[weight]=model
+models={k:models[k] for k in sorted(models)}
+
+# %%
+# Losses of all models with different weight settings
+losses=['reconstruction_loss_train','kl_local_train', 'z_distance_cycle_train']
+epoch_detail=20
+for weight,model in models.items():
+    fig,axs=plt.subplots(2,len(losses),figsize=(len(losses)*3,4))
+    fig.suptitle(str(weight))
+    for ax_i,l in enumerate(losses):
+        axs[0,ax_i].plot(
+            model.trainer.logger.history[l].index,
+            model.trainer.logger.history[l][l])
+        axs[0,ax_i].set_title(l)
+        axs[1,ax_i].plot(
+            model.trainer.logger.history[l].index[epoch_detail:],
+            model.trainer.logger.history[l][l][epoch_detail:])
+    fig.tight_layout()
+
+# %%
+# Embeddings of different models
+embeds={}
+for weight,model in models.items():
+    embeds[weight] = model.embed(
+        adata=adata_training,
+        indices=None,
+        batch_size=None,
+        as_numpy=True)
+
+# %%
+for weight,embed in embeds.items():
+    fig,ax=plt.subplots()
+    plt.violinplot(embeds[weight])
+    ax.set_title(weight)
+
+# %% [markdown]
+# Since the span of features is not informative about their variability look at how they vary accross cts. To make this less afected by system do only for human data. Normalise features to [0,1] before averaging per ct to ensure that feature size does not affect the perceived variability. Then compute var of feature accross ct means.
+
+# %%
+ct_var={}
+ct_entropy={}
+for weight,embed in embeds.items():
+    # Human cells
+    cells= adata_training.obs.system==1
+    embed_sub=embed[cells,:]
+    # Minmax scale the embed comonents so that perceived var is not affected by range
+    x=pd.DataFrame(minmax_scale(embed_sub),index=adata_training.obs_names[cells])
+    #var_idx=x.var().sort_values(ascending=False).index
+    x['cell_type']=adata_training[x.index,:].obs['cell_type_final']
+    x=x.groupby('cell_type').mean()
+    #x=x[var_idx]
+    g=sb.clustermap(x,cmap='cividis',figsize=(7,5),#col_cluster=False
+                   )
+    g.fig.suptitle(weight)
+    ct_var[weight]=x.var()
+    ct_entropy[weight]=entropy(x)
+ct_var=pd.DataFrame(ct_var)
+ct_entropy=pd.DataFrame(ct_entropy)
+fig,ax=plt.subplots(1,2,figsize=(8,3))
+ct_var.boxplot(ax=ax[0])
+ax[0].set_title('var over cts')
+ct_entropy.boxplot(ax=ax[1])
+ax[1].set_title('entropy over cts')
 
 # %%
 for weight,embed in embeds.items():
