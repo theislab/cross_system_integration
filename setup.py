a19b3e8d58928b8ec22bb6423dd188d3aa75d970
Hrovatin <karin.hrovatin@gmail.com>
2023-03-21 13:08:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 
 import setuptools
 
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 if __name__ == "__main__":
     setuptools.setup(name="cross_species_prediction",
+                     packages=['cross_species_prediction', 'constraint_pancreas_example'],
                      install_requires=requirements)
